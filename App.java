f691a4b69e8b8753bedf7f0baef755cb8253a32d
taran3aggarwal <taran3aggarwal@gmail.com>
2023-12-16 17:11:59 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.wissen.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World By Taran Aggarwal!" );
+    }
+}
